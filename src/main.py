de29bc2e0562de9ae067f81eca967bbf36cb177c
Madaspe <danil.treshchev@yandex.ru>
2021-03-27 00:40:04 +0500

    Add src folder with source

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from prompt_toolkit.shortcuts import input_dialog
+from settings import TOKEN, save_token
+from tui import start_tui
+
+if TOKEN is None:
+    result = input_dialog(
+        title="VK ", text="Please type your vk token:      (Tab to change focus)"
+    ).run()
+
+    if result:
+        TOKEN = result
+        save_token(TOKEN)
+
+start_tui()
+
